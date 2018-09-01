78933e0d165acaec93e0caad8b40b3e7d7778b10
markap <mart.kapfhammer@gmail.com>
2018-09-01 22:35:00 +0200

    adding logic for last frame, more tests

--- api/models.py
+++ api/models.py
@@ -3,6 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class GameOverException(Exception):
+    pass
+
+class InvalidScoreException(Exception):
+    pass
+
 
 class Frame(models.Model):
     game = models.ForeignKey('Game', related_name='frames') # more attributes?
@@ -11,6 +17,7 @@ class Frame(models.Model):
 
     is_spare = models.BooleanField(default=False)
     is_strike = models.BooleanField(default=False)
+    is_last_frame = models.BooleanField(default=False)
 
     score_one = models.IntegerField(null=True, blank=True)#min max
     score_two = models.IntegerField(null=True, blank=True)#min max
@@ -24,53 +31,77 @@ class Frame(models.Model):
                     self.score_two or 0,
                     self.score_three or 0])
 
+    def get_frame_score_for_strike(self):
+        return sum([self.score_one or 0,
+                    self.score_two or 0])
+
     def is_closed(self):
-        return self.is_strike or (self.score_one and self.score_two)
+        return not self.is_last_frame and (self.is_strike or (self.score_one is not None and self.score_two is not None))
 
 
 
 class Game(models.Model):
-    frame_count = models.IntegerField(null=True, blank=True)#min max
+    is_over = models.BooleanField(default=False)
 
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
     def add_score(self, score):
-        # check if frame is below 10 to create new one
-        # check if game is over
-        # if last frame and spare or strike, allow more balls
+        if self.is_over:
+            raise GameOverException()
 
-        # check score between 0 and 10
-        # value error
+        if score < 0 or score > 10 or not isinstance(score, (int, long)): # todo function
+            raise InvalidScoreException()
 
 
         prev_frame = self.frames.last()
 
-        # no prev frame or prev frame closed -> create new frame
-        if prev_frame is None or (prev_frame and prev_frame.is_closed()): #and not prev_frame.is_last_frame): #todo improve
+        # there is no frame yet 
+        # or the previous frame is already closed, 
+        # so let's create a new frame
+        # check if it's the last frame and if there is a strike
+        # and add the score
+        if prev_frame is None or (prev_frame and prev_frame.is_closed()): 
             Frame.objects.create(
                 game=self,
                 score_one=score,
                 is_strike=self._is_strike(score),
-                #is_last_frame=self._is_last_frame()
+                is_last_frame=self._adding_last_frame()
             )
-        #elif prev_frame.is_last_frame and strike:
-        #elif prev_frame.is_last_frame and spare:
+
+
+        # we playing in the last frame and we have either a spare or a strike
+        # if it's round three add and close the game, otherwise leave the game open
+        elif prev_frame.is_last_frame and (prev_frame.is_strike or prev_frame.is_spare):
+            if prev_frame.score_two is None:
+                prev_frame.score_two = score
+                prev_frame.save()
+            else:
+                prev_frame.score_three = score
+                prev_frame.save()
+
+                self.is_over = True
+                self.save()
             
+        # the typical second shot 
+        # check if it is a spare and if it's the last frame 
+        # and it's not a spare the game is over
         else:
             # update prev frame
+            if prev_frame.score_one + score > 10: # function
+                raise InvalidScoreException()
+
             prev_frame.score_two = score
             prev_frame.is_spare = self._is_spare(prev_frame.score_one, score)
             prev_frame.save()
 
-            # if last_frame check if is_strike or is_spare 
-
-            #if last_frame.is_last_frame():
-            #    self.is_over = True
-            #    self.save()
+            if prev_frame.is_last_frame and not prev_frame.is_spare:
+                self.is_over = True
+                self.save()
 
 
-    def _is_last_frame(self):
+    def _adding_last_frame(self):
+        print self.frames.count()
         return self.frames.count() == 9 # todo const
     
     def _is_strike(self, score):
@@ -84,14 +115,11 @@ class Game(models.Model):
         return False
 
 
-
-        
-
     def calculate_score(self):
         frame_list = [frame for frame in self.frames.all()]
         frame_list_calculated = []
 
-        def get_next_frame(frame_list, index):
+        def _get_next_frame(frame_list, index):
             try:
                 next_frame = frame_list[index]
             except IndexError:
@@ -101,22 +129,21 @@ class Game(models.Model):
         for index, frame in enumerate(frame_list):
             score = frame.get_frame_score()
 
-            next_frame = get_next_frame(frame_list, index+1)
+            next_frame = _get_next_frame(frame_list, index+1)
             if frame.is_spare and next_frame:
                 score += next_frame.get_score_one()
             
             elif frame.is_strike and next_frame:
-                score += next_frame.get_frame_score()
+                score += next_frame.get_frame_score_for_strike()
 
-                next_next_frame = get_next_frame(frame_list, index+2)
+                next_next_frame = _get_next_frame(frame_list, index+2)
                 if next_frame.is_strike and next_next_frame:
                     score += next_next_frame.get_score_one()
 
             frame_list_calculated.append(score)
-            # instead of frame_list, set an attribute in the frame
+            frame_list[index].score = sum(frame_list_calculated)
             
-        print frame_list_calculated
-        return sum(frame_list_calculated)
+        return sum(frame_list_calculated), frame_list
 
 
                 
--- api/tests.py
+++ api/tests.py
@@ -9,6 +9,20 @@ class AddScoreTest(TestCase):
     def setUp(self):
         self.game = models.Game.objects.create()
 
+   
+    # helper function, mainly to test the various
+    # cases how a game can end
+    def _create_strike_frames(self, count):
+        for i in xrange(count):
+            self.game.add_score(10)
+
+    # helper function to check frames score
+    def _assert_frames(self, frames, expected):
+        for index, frame in enumerate(frames):
+            print "check frame %s - %s" % (frame.score, expected[index])
+            self.assertEqual(frame.score, expected[index])
+
+
     def test_add_score(self):
         self.assertEqual(self.game.frames.count(), 0)
         self.game.add_score(6)
@@ -27,29 +41,287 @@ class AddScoreTest(TestCase):
         self.game.add_score(3)
         self.assertEqual(self.game.frames.count(), 4)
 
+    def test_invalid_score(self):
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, 11)
+
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, -1)
+
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, 0.5)
+
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, "hihi")
+
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, None)
+
+    def test_invalid_score_more_than_10(self):
+        self.game.add_score(6)
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, 5)
+        self.game.add_score(4)
+
+    def test_invalid_score_end_of_game(self):
+        self._create_strike_frames(9)
+        self.assertFalse(self.game.is_over)
+        self.assertEqual(self.game.frames.count(), 9)
+
+        self.game.add_score(3)
+        self.assertRaises(models.InvalidScoreException,
+                          self.game.add_score, 8)
+
+        self.assertFalse(self.game.is_over)
+        self.game.add_score(7)
+        self.game.add_score(7)
+
+        self.assertEqual(self.game.frames.count(), 10)
+        self.assertTrue(self.game.is_over)
+
+    def test_valid_score_end_of_game_strike(self):
+        self._create_strike_frames(10)
+        self.assertFalse(self.game.is_over)
+        self.assertEqual(self.game.frames.count(), 10)
+
+        self.game.add_score(7)
+        self.game.add_score(7)
+        self.assertTrue(self.game.is_over)
+
+    def test_lame_finish(self):
+        self._create_strike_frames(9)
+        self.assertFalse(self.game.is_over)
+        self.assertEqual(self.game.frames.count(), 9)
+
+        self.game.add_score(3)
+        self.assertEqual(self.game.frames.count(), 10)
+        self.game.add_score(4)
+        self.assertEqual(self.game.frames.count(), 10)
+
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 257)
+
+        self._assert_frames(
+            frames, 
+            [30, 60, 90, 120, 150, 180, 210, 233, 250, 257]
+        )
+
+        self.assertRaises(models.GameOverException, self.game.add_score, 4)
+        self.assertTrue(self.game.is_over)
+
+    def test_spare_finish(self):
+        self._create_strike_frames(9)
+
+        self.game.add_score(3)
+        self.assertEqual(self.game.frames.count(), 10)
+        self.game.add_score(7)
+        self.assertEqual(self.game.frames.count(), 10)
+        self.game.add_score(5)
+        self.assertEqual(self.game.frames.count(), 10)
+
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 268)
+        self._assert_frames(
+            frames,
+            [30, 60, 90, 120, 150, 180, 210, 233, 253, 268]
+        )
+
+        self.assertRaises(models.GameOverException, self.game.add_score, 10)
+        self.assertTrue(self.game.is_over)
+
+    def test_perfect_game(self):
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.game.add_score(10)
+        self.assertEqual(self.game.frames.count(), 10)
+        self.game.add_score(10)
+        self.assertEqual(self.game.frames.count(), 10)
+        self.game.add_score(10)
+        self.assertEqual(self.game.frames.count(), 10)
+
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 300)
+        self._assert_frames(
+            frames, 
+            [30, 60, 90, 120, 150, 180, 210, 240, 270, 300]
+        )
+
+        self.assertRaises(models.GameOverException, self.game.add_score, 10)
+
+    def test_calculate_without_frame(self):
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 0)
+        self.assertEqual(len(frames), 0)
+       
+       
+    def test_random_game(self): 
+        self.game.add_score(2)
+        self.game.add_score(8)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 10)
+        self._assert_frames(
+            frames,
+            [10]
+        )
+        
+        self.game.add_score(2)
+        self.game.add_score(8)
+   
+        self.game.add_score(6)
+        self.game.add_score(2)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 36)
+        self._assert_frames(
+            frames,
+            [12, 28, 36]
+        )
+
+        self.game.add_score(10)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 46)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 46]
+        )
+   
+        self.game.add_score(4)
+        self.game.add_score(5)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 64)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64]
+        )
+
+        self.game.add_score(3)
+        self.game.add_score(7)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 74)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64, 74]
+        )
+
+        self.game.add_score(10)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 94)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64, 84, 94]
+        )
+
+        self.game.add_score(0)
+        self.game.add_score(0)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 94)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64, 84, 94, 94]
+        )
+
+        self.game.add_score(5)
+        self.game.add_score(5)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 104)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64, 84, 94, 94, 104]
+        )
+
+        self.game.add_score(4)
+        self.assertFalse(self.game.is_over)
+        self.game.add_score(3)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 115)
+        self._assert_frames(
+            frames,
+            [12, 28, 36, 55, 64, 84, 94, 94, 108, 115]
+        )
+
+        self.assertTrue(self.game.is_over)
+
+
+    def test_horrible_game(self):
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        score, _ = self.game.calculate_score()
+        self.assertEqual(score, 0)
+        self.assertTrue(self.game.is_over)
+
+
+    def test_empty_frame(self): 
+
+        self.game.add_score(10)
+        self.game.add_score(0)
+        self.game.add_score(0)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(score, 10)
+        self._assert_frames(
+            frames,
+            [10, 10]
+        )
+        self.assertEqual(self.game.frames.count(), 2)
+
+
+        self.game.add_score(5)
+        self.game.add_score(5)
+        score, frames = self.game.calculate_score()
+        self.assertEqual(self.game.frames.count(), 3)
+        self.assertEqual(score, 20)
+        self._assert_frames(
+            frames,
+            [10, 10, 20]
+        )
+
+
 class CalculateScoreTest(TestCase):
 	def setUp(self):
 		self.game = models.Game.objects.create()
 
 	def test_calculate_lame(self):
 		self.game.add_score(3)
-		self.assertEqual(self.game.calculate_score(), 3)
+		self.assertEqual(self.game.calculate_score()[0], 3)
 		self.game.add_score(5)
-		self.assertEqual(self.game.calculate_score(), 8)
+		self.assertEqual(self.game.calculate_score()[0], 8)
 
 		self.game.add_score(5)
 		self.game.add_score(5)
-		self.assertEqual(self.game.calculate_score(), 18)
-
-		self.game.add_score(10)
-		self.assertEqual(self.game.calculate_score(), 38)
+		self.assertEqual(self.game.calculate_score()[0], 18)
 
 		self.game.add_score(10)
-		self.assertEqual(self.game.calculate_score(), 58)
+		self.assertEqual(self.game.calculate_score()[0], 38)
+
+		self.game.add_score(10)
+		self.assertEqual(self.game.calculate_score()[0], 58)
 
 		self.game.add_score(0)
 		self.game.add_score(3)
-		self.assertEqual(self.game.calculate_score(), 64)
+		self.assertEqual(self.game.calculate_score()[0], 64)
         
 
 
